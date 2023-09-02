b373a37770f64e70855e3ab4eb5e71de2bb313fc
SUITE <SUITE@SUITEE206-ST09.cibertec.edu.pe>
2023-09-02 12:47:40 -0500

    solucion a tarea de markitos

---
+++ src/main/java/com/empresa/service/AlumnoServiceImpl.java
@@ -0,0 +1,23 @@
+package com.empresa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.empresa.entity.Alumno;
+import com.empresa.repository.AlumnoRepository;
+
+
+@Service
+public class AlumnoServiceImpl implements AlumnoService {
+
+	
+	
+	@Autowired
+	private AlumnoRepository repository;
+	
+	@Override
+	public Alumno insertAlumno(Alumno obj) {
+		return repository.save(obj);
+	}
+
+}
